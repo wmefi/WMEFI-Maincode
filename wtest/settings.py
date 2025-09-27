6eb2cdf221f9d08f832984ec4d716598b7ae9743
wmefi <connect@wmefi.co.in>
2025-09-27 18:17:29 +0530

    host

--- wtest/settings.py
+++ wtest/settings.py
@@ -26,7 +26,8 @@ SECRET_KEY = 'django-insecure-c$-2hc3l=(l9841mb0ldi!(#kx%tk3i+o3!px1^h97tar$#-u=
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['wtest-1.onrender.com', '127.0.0.1', 'localhost']
+
 
 
 # Application definition
